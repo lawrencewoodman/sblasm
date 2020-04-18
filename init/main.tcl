50f1cac3d09b80163bab1093346fb03bc72cdff3
Lawrence Woodman <lwoodman@vlifesystems.com>
2020-04-18 08:09:25 +0100

    Add TODO: to use tekyll header

--- init/main.tcl
+++ init/main.tcl
@@ -1,4 +1,5 @@
 set header "# File created using tekyll: https://github.com/lawrencewoodman/tekyll\n\n"
+# TODO: Use header above
 set mainTcl [read -directory [dir root] main.tcl]
 set uncommentedOrnamentText [regsub -line -all {^#>(.*)$} $mainTcl {\1}]
 set subleasmTcl [append $header [ornament $uncommentedOrnamentText]]
