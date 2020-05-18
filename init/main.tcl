c4f62727b9e9f5aa36873f269c02323e1edc760d
Lawrence Woodman <lwoodman@vlifesystems.com>
2020-05-18 09:06:46 +0100

    Use header for tekyll and make build file sblasm.tcl

--- init/main.tcl
+++ init/main.tcl
@@ -1,6 +1,6 @@
 set header "# File created using tekyll: https://github.com/lawrencewoodman/tekyll\n\n"
-# TODO: Use header above
 set mainTcl [read -directory [dir root] main.tcl]
 set uncommentedOrnamentText [regsub -line -all {^#>(.*)$} $mainTcl {\1}]
-set subleasmTcl [append $header [ornament $uncommentedOrnamentText]]
-write [file join [dir build] subleasm.tcl] $subleasmTcl
+set sblasmTcl $header
+append sblasmTcl [ornament $uncommentedOrnamentText]
+write [file join [dir build] sblasm.tcl] $sblasmTcl
