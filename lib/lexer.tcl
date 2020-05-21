ef8714805feb28f972860f18ca6b48d1002495e7
Lawrence Woodman <lwoodman@vlifesystems.com>
2020-05-21 08:02:23 +0100

    Ensure string, number and expression can't be first token on line

--- lib/lexer.tcl
+++ lib/lexer.tcl
@@ -15,6 +15,7 @@ xproc::proc lex {src} {
   set lineNum 1
   foreach line $src {
     set linePos 0
+    set numLineTokens 0
     while {$linePos != -1 && $linePos < [string length $line]} {
       set restLine [string range $line $linePos end]
       switch -regexp -matchvar matches -indexvar indices -- $restLine {
@@ -27,14 +28,21 @@ xproc::proc lex {src} {
           set directive [string trimright [lindex $matches 0]]
           lappend tokens [list directive $directive $lineNum]
           incr linePos [lindex [lindex $indices 0] 1]
+          incr numLineTokens
         }
         {(^"([^\\"]|\\.)*"\s+)|(^"([^\\"]|\\.)*"$)} {
           # String
-          # TODO: Check isn't first token on line
-          set str [string trimright [lindex $matches 0]]
-          set str [string range $str 1 end-1]
-          lappend tokens [list string $str $lineNum]
+          if {$numLineTokens == 0} {
+            set err [dict create lineNum $lineNum line $line \
+                                 msg "Invalid position for string"]
+            lappend errors $err
+          } else {
+            set str [string trimright [lindex $matches 0]]
+            set str [string range $str 1 end-1]
+            lappend tokens [list string $str $lineNum]
+          }
           incr linePos [lindex [lindex $indices 0] 1]
+          incr numLineTokens
         }
         {(^[a-zA-Z][a-zA-Z_0-9:]*:\s+)|(^[a-zA-Z][a-zA-Z_0-9:]*:$)} {
           # Label
@@ -50,32 +58,47 @@ xproc::proc lex {src} {
             lappend tokens [list label $label $lineNum]
           }
           incr linePos [lindex [lindex $indices 0] 1]
+          incr numLineTokens
         }
         {^[a-zA-Z0-9:$]+[+-]+[$a-zA-Z0-9()+\-:]+} {
           # Expression
-          # TODO: Check isn't first token on line
-          set expr [string trim [lindex $matches 0]]
-          lappend tokens [list expr $expr $lineNum]
+          if {$numLineTokens == 0} {
+            set err [dict create lineNum $lineNum line $line \
+                                 msg "Invalid position for expression"]
+            lappend errors $err
+          } else {
+            set expr [string trim [lindex $matches 0]]
+            lappend tokens [list expr $expr $lineNum]
+          }
           incr linePos [lindex [lindex $indices 0] 1]
+          incr numLineTokens
         }
         {(^[a-zA-Z$][$a-zA-Z0-9_:]*\s+)|(^[a-zA-Z$][$a-zA-Z0-9_:]*$)} {
           # Identifier
           set id [string trimright [lindex $matches 0]]
           lappend tokens [list id $id $lineNum]
           incr linePos [lindex [lindex $indices 0] 1]
+          incr numLineTokens
         }
         {(^[-]?[0-9]+\s+)|(^[-]?[0-9]+$)} {
           # Number
-          # TODO: Check isn't first token on line
-          set num [string trim [lindex $matches 0]]
-          lappend tokens [list num $num $lineNum]
+          if {$numLineTokens == 0} {
+            set err [dict create lineNum $lineNum line $line \
+                                 msg "Invalid position for number"]
+            lappend errors $err
+          } else {
+            set num [string trim [lindex $matches 0]]
+            lappend tokens [list num $num $lineNum]
+          }
           incr linePos [lindex [lindex $indices 0] 1]
+          incr numLineTokens
         }
         {^;.*$} {
           # Comment
           set comment [string trim [string range [lindex $matches 0] 1 end]]
           lappend tokens [list comment $comment $lineNum]
           incr linePos [lindex [lindex $indices 0] 1]
+          incr numLineTokens
         }
         default {
           # TODO: Better error message?
